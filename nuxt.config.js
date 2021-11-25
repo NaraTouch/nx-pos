7cf4a169ce7b1af825cf70a54624564ed1a6bbb5
NaraTouch <sophonaratouch@gmail.com>
2021-11-25 17:37:16 +0700

    change port

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,69 +1,72 @@
 
 export default {
-  generate: {
-    fallback: true
-  },
+    server: {
+        port: 8080 // default: 3000
+    },
+    generate: {
+        fallback: true
+    },
 
-  // Global page headers (https://go.nuxtjs.dev/config-head)
-  head: {
-      title: 'Givest',
-      titleTemplate: '%s || Givest',
-      meta: [
-          { charset: 'utf-8' },
-          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-          { hid: 'description', name: 'description', content: '' }
-      ],
-      link: [
-          { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      ],
-  },
+    // Global page headers (https://go.nuxtjs.dev/config-head)
+    head: {
+        title: 'Givest',
+        titleTemplate: '%s || Givest',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        ],
+    },
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-      '~/assets/scss/style.scss'
-  ],
+    // Global CSS (https://go.nuxtjs.dev/config-css)
+    css: [
+        '~/assets/scss/style.scss'
+    ],
 
-  router: {
-      linkExactActiveClass: 'active',
-  },
+    router: {
+        linkExactActiveClass: 'active',
+    },
 
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-      '~/plugins/vue-awesome-swiper.js',
-      {
-          src: "~/plugins/aos", 
-          ssr: false 
-      },
-      { 
-          src: '~plugins/vue-backtotop.js', 
-          ssr: false 
-      },
-  ],
+    // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+    plugins: [
+        '~/plugins/vue-awesome-swiper.js',
+        {
+            src: "~/plugins/aos", 
+            ssr: false 
+        },
+        { 
+            src: '~plugins/vue-backtotop.js', 
+            ssr: false 
+        },
+    ],
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
-  components: true,
+    // Auto import components (https://go.nuxtjs.dev/config-components)
+    components: true,
 
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-  ],
+    // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+    buildModules: [
+    ],
 
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-      // https://go.nuxtjs.dev/bootstrap
-      'bootstrap-vue/nuxt',
-      '@nuxtjs/style-resources',
-  ],
+    // Modules (https://go.nuxtjs.dev/config-modules)
+    modules: [
+        // https://go.nuxtjs.dev/bootstrap
+        'bootstrap-vue/nuxt',
+        '@nuxtjs/style-resources',
+    ],
 
-  styleResources: {
-      scss: [
-          '~/assets/scss/_variables.scss',
-      ]
-  },
+    styleResources: {
+        scss: [
+            '~/assets/scss/_variables.scss',
+        ]
+    },
 
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-      extractCSS: true,
-      extend (config, ctx) {
-      },
-  },
+    // Build Configuration (https://go.nuxtjs.dev/config-build)
+    build: {
+        extractCSS: true,
+        extend (config, ctx) {
+        },
+    },
 }
